d4a9fad05e14c17ca5b2eaaf3d14564385d482b5
mitsuhiro <papu.m0917@icloud.com>
2021-01-21 05:53:57 +0900

    ルート変更

--- routes/web.php
+++ routes/web.php
@@ -42,26 +42,26 @@ Route::get('/home', 'HomeController@index')->name('home');
 | 3) Admin 認証不要
 |--------------------------------------------------------------------------
 */
-// Route::group(['prefix' => 'admin'], function () {
-//     Route::get('/', function () {
-//         return redirect('/admin/home');
-//     });
-//     Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
-//     Route::post('login', 'Admin\LoginController@login');
-// });
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', function () {
+        return redirect('/admin/home');
+    });
+    Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+    Route::post('login', 'Admin\LoginController@login');
+});
 
 /*
 |--------------------------------------------------------------------------
 | 4) Admin ログイン後
 |--------------------------------------------------------------------------
 */
-// Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
-Route::post('logout', 'Admin\LoginController@logout')->name('admin.logout');
-Route::get('home', 'Admin\HomeController@index')->name('admin.home');
-Route::get('admin/index', 'Admin\AdminController@index')->name('admin.index');
-Route::get('create', 'Admin\AdminController@create')->name('admin.create');
-Route::post('store', 'Admin\AdminController@store')->name('admin.store');
-Route::get('edit', 'Admin\AdminController@edit')->name('admin.edit');
-Route::post('edit', 'Admin\AdminController@update')->name('admin.edit');
-Route::post('delete', 'Admin\AdminController@delete')->name('admin.delete');
-// });
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
+    Route::post('logout', 'Admin\LoginController@logout')->name('admin.logout');
+    Route::get('home', 'Admin\HomeController@index')->name('admin.home');
+    Route::get('index', 'Admin\AdminController@index')->name('admin.index');
+    Route::get('create', 'Admin\AdminController@create')->name('admin.create');
+    Route::post('store', 'Admin\AdminController@store')->name('admin.store');
+    Route::get('edit', 'Admin\AdminController@edit')->name('admin.edit');
+    Route::post('edit', 'Admin\AdminController@update')->name('admin.edit');
+    Route::post('delete', 'Admin\AdminController@delete')->name('admin.delete');
+});
