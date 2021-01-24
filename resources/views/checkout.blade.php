09fab982fc128190baaecf360940f0034fcd511f
mitsuhiro <papu.m0917@icloud.com>
2021-01-24 20:33:53 +0900

    修正

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-
-
     <div class="container-fluid">
         <div class="">
             <div class="mx-auto" style="max-width:1200px">
                 <h1 class="text-center font-weight-bold" style="color:#555555;  font-size:1.2em; padding:24px 0px;">
                     {{ Auth::user()->name }}さんご購入ありがとうございました!
                 </h1>
+                <p class="text-center mt-5">決済画面へ進んでください。お手続き完了次第商品を発送致します。</p>
                 <form action="{{ asset('payment') }}" method="POST" class="text-center mt-5">
                     {{ csrf_field() }}
                     <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
@@ -18,14 +17,7 @@
                         data-currency="JPY">
                     </script>
                 </form>
-
-                <p class="text-center mt-5">決済画面へ進んでください。お手続き完了次第商品を発送致します。</p>
-                {{-- <a href="/">商品一覧へ</a> --}}
-
-                {{-- <p class="text-center mt-5"><a class="btn btn-primary"
-                        href="/index">決済画面へ</a></p> --}}
             </div>
         </div>
     </div>
-
 @endsection
