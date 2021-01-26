b1618cc76a888d8f2052186697ed420449862cb4
mitsuhiro <papu.m0917@icloud.com>
2021-01-26 22:07:48 +0900

    LaravelからMyShopへと変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'MyShop') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -60,7 +60,7 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
-                                                                                                                                                                                                                                           document.getElementById('logout-form').submit();">
+                                                                                                                                                                                                                                               document.getElementById('logout-form').submit();">
                                         {{ __('ログアウト') }}
                                     </a>
 
